af1692160523bda4788c148837e7b2cdbcf46acb
Olivier Cailloux <olivier.cailloux@gmail.com>
2020-05-05 17:50:48 +0200

    Public columns

--- src/main/java/io/github/oliviercailloux/samples/chess/MyChessBoard.java
+++ src/main/java/io/github/oliviercailloux/samples/chess/MyChessBoard.java
@@ -1,6 +1,9 @@
 package io.github.oliviercailloux.samples.chess;
 
+import com.google.common.collect.ImmutableList;
+
 public class MyChessBoard implements ChessBoard {
+	public static final ImmutableList<String> COLUMNS = ImmutableList.of("a", "b", "c", "d", "e", "f", "g", "h");
 
 	/**
 	 * <p>
